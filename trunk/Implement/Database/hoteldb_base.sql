02c634051bc3f387a74876f2647c7c41185c56c9
tuonghuy09 <tuonghuy09@ae9a0e02-e63f-11de-b362-3513cf04e977>
2009-12-16 18:44:29 +0000

    add project client modify DB

--- trunk/Implement/Database/hoteldb_base.sql
+++ trunk/Implement/Database/hoteldb_base.sql
@@ -1,4 +1,4 @@
-﻿CREATE DATABASE IF NOT EXISTS `HOTELDB`;
+CREATE DATABASE IF NOT EXISTS `HOTELDB`;
 
 USE `HOTELDB`;
 
@@ -10,6 +10,7 @@ DROP TABLE IF EXISTS `TINH_TRANG_PHONG`;
 DROP TABLE IF EXISTS `LOAI_PHONG`;
 DROP TABLE IF EXISTS `KHACH_HANG`;
 DROP TABLE IF EXISTS `LOAI_THUE`;
+DROP TABLE IF EXISTS `LOAI_KHACH_HANG`;
 
 
 -- Tao bang PHONG
@@ -25,18 +26,28 @@ CREATE TABLE `PHONG` (
 )
 ENGINE = InnoDB;
 
--- Tao bang KHACH_HANG
-CREATE TABLE `KHACH_HANG` (
-  `id` INTEGER, -- cmnd/visa
-  `ten` TEXT NOT NULL,
-  `dia_chi` TEXT NOT NULL,
-  `dien_thoai` TEXT NOT NULL,
-  `ngay_sinh` DATETIME NOT NULL,
-  `quoc_tich` TEXT,
+-- Tao bang LOAI KHACH HANG
+CREATE TABLE  `hoteldb`.`loai_khach_hang` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `loai` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-  PRIMARY KEY(`id`)
-)
-ENGINE = InnoDB;
+
+-- Tao bang KHACH_HANG
+DROP TABLE IF EXISTS `hoteldb`.`khach_hang`;
+CREATE TABLE  `hoteldb`.`khach_hang` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ten` text NOT NULL,
+  `dia_chi` text NOT NULL,
+  `dien_thoai` text NOT NULL,
+  `ngay_sinh` datetime NOT NULL,
+  `id_loai_khach_hang` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_khach_hang_Loai_khach_hang` (`id_loai_khach_hang`),
+  CONSTRAINT `FK_khach_hang_Loai_khach_hang` FOREIGN KEY (`id_loai_khach_hang`) REFERENCES `loai_khach_hang` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 
 -- Tao bang LOAI_THUE
 CREATE TABLE `LOAI_THUE` (
@@ -95,24 +106,25 @@ ENGINE = InnoDB;
 ALTER TABLE `hoteldb`.`phong`
  ADD CONSTRAINT `FK_PHONG_TINH_TRANG` FOREIGN KEY `FK_PHONG_TINH_TRANG` (`id_tinh_trang`)
     REFERENCES `tinh_trang_phong` (`id`)
-    ON DELETE RESTRICT
-    ON UPDATE RESTRICT,
+    ON DELETE CASCADE 
+    ON UPDATE CASCADE ,
  ADD CONSTRAINT `FK_PHONG_LOAI_PHONG` FOREIGN KEY `FK_PHONG_LOAI_PHONG` (`id_loai_phong`)
     REFERENCES `loai_phong` (`id`)
-    ON DELETE RESTRICT
-    ON UPDATE RESTRICT;
+    ON DELETE CASCADE 
+    ON UPDATE CASCADE ;
 
 
 ALTER TABLE `hoteldb`.`thue_phong`
  ADD CONSTRAINT `FK_THUE_PHONG_PHONG` FOREIGN KEY `FK_THUE_PHONG_PHONG` (`id_phong`)
     REFERENCES `phong` (`id`)
-    ON DELETE RESTRICT
-    ON UPDATE RESTRICT,
+    ON DELETE CASCADE 
+    ON UPDATE CASCADE ,
  ADD CONSTRAINT `FK_THUE_PHONG_KHACH_HANG` FOREIGN KEY `FK_THUE_PHONG_KHACH_HANG` (`id_khach_hang`)
     REFERENCES `khach_hang` (`id`)
-    ON DELETE RESTRICT
-    ON UPDATE RESTRICT,
+    ON DELETE CASCADE 
+    ON UPDATE CASCADE ,
  ADD CONSTRAINT `FK_THUE_PHONG_LOAI_THUE` FOREIGN KEY `FK_THUE_PHONG_LOAI_THUE` (`id_loai_thue`)
     REFERENCES `loai_thue` (`id`)
-    ON DELETE RESTRICT
-    ON UPDATE RESTRICT;
+    ON DELETE CASCADE 
+    ON UPDATE CASCADE;
+
