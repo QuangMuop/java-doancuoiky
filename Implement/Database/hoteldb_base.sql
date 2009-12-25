862757eb098bbb9432d5ba6f229ec2848dea4eac
hvu.nguyen2007 <hvu.nguyen2007@ae9a0e02-e63f-11de-b362-3513cf04e977>
2009-12-25 15:18:07 +0000

    Insert DB

--- Implement/Database/hoteldb_base.sql
+++ Implement/Database/hoteldb_base.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE IF NOT EXISTS `HOTELDB`;
+﻿CREATE DATABASE IF NOT EXISTS `HOTELDB`;
 
 USE `HOTELDB`;
 
@@ -15,8 +15,8 @@ DROP TABLE IF EXISTS `LOAI_KHACH_HANG`;
 
 -- Tao bang PHONG
 CREATE TABLE `PHONG` (
-  `id` INTEGER,
-  `gia` INTEGER NOT NULL,
+  `id` INTEGER AUTO_INCREMENT,
+  `gia` INTEGER,
   `id_tinh_trang` INTEGER NOT NULL,
   `id_loai_phong` INTEGER NOT NULL,
   `hinh_anh` TEXT,
@@ -27,7 +27,7 @@ CREATE TABLE `PHONG` (
 ENGINE = InnoDB;
 
 -- Tao bang LOAI KHACH HANG
-CREATE TABLE  `hoteldb`.`loai_khach_hang` (
+CREATE TABLE  `LOAI_KHACH_HANG` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `loai` int(10) unsigned NOT NULL,
   PRIMARY KEY (`id`)
@@ -35,8 +35,7 @@ CREATE TABLE  `hoteldb`.`loai_khach_hang` (
 
 
 -- Tao bang KHACH_HANG
-DROP TABLE IF EXISTS `hoteldb`.`khach_hang`;
-CREATE TABLE  `hoteldb`.`khach_hang` (
+CREATE TABLE  `KHACH_HANG` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `ten` text NOT NULL,
   `gioi_tinh` text,
@@ -52,7 +51,7 @@ CREATE TABLE  `hoteldb`.`khach_hang` (
 
 -- Tao bang LOAI_THUE
 CREATE TABLE `LOAI_THUE` (
-  `id` INTEGER,
+  `id` INTEGER AUTO_INCREMENT,
   `loai` TEXT,    -- Dat phong/Thue phong
 
   PRIMARY KEY(`id`)
@@ -61,9 +60,9 @@ ENGINE = InnoDB;
 
 -- Tao bang THUE_PHONG
 CREATE TABLE `THUE_PHONG` (
-  `id_phong` INTEGER,
-  `id_khach_hang` INTEGER,
-  `ngay_thue` DATETIME,
+  `id_phong` INTEGER NOT NULL,
+  `id_khach_hang` INTEGER NOT NULL,
+  `ngay_thue` DATETIME NOT NULL,
   `ngay_tra` DATETIME,
   `tong_gia` INTEGER,
   `id_loai_thue` INTEGER NOT NULL,
@@ -74,7 +73,7 @@ ENGINE = InnoDB;
 
 -- Tao bang TINH_TRANG_PHONG
 CREATE TABLE `TINH_TRANG_PHONG` (
-  `id` INTEGER,
+  `id` INTEGER AUTO_INCREMENT,
   `ten` TEXT NOT NULL,    -- Da thue/Con trong
 
   PRIMARY KEY(`id`)
@@ -83,7 +82,7 @@ ENGINE = InnoDB;
 
 -- Tao bang LOAI_PHONG
 CREATE TABLE `LOAI_PHONG` (
-  `id` INTEGER,
+  `id` INTEGER AUTO_INCREMENT,
   `ten` TEXT NOT NULL,
   `gia` INTEGER NOT NULL,
 
@@ -126,6 +125,123 @@ ALTER TABLE `hoteldb`.`thue_phong`
     ON UPDATE CASCADE ,
  ADD CONSTRAINT `FK_THUE_PHONG_LOAI_THUE` FOREIGN KEY `FK_THUE_PHONG_LOAI_THUE` (`id_loai_thue`)
     REFERENCES `loai_thue` (`id`)
-    ON DELETE CASCADE 
+    ON DELETE CASCADE
     ON UPDATE CASCADE;
 
+
+-- Insert data
+-- LOAI_KHACH_HANG
+INSERT INTO `LOAI_KHACH_HANG`(`id`, `loai`) VALUES
+  (2, 0);
+INSERT INTO `LOAI_KHACH_HANG`(`id`, `loai`) VALUES
+  (1, 1);
+
+
+-- KHACH_HANG
+INSERT INTO `KHACH_HANG`(`id`, `ten`, `gioi_tinh`, `dia_chi`, `dien_thoai`, `ngay_sinh`, `id_loai_khach_hang`) VALUES
+  (10, 'Ly Thuong Kiet', 'Nam', 'Dia chi', 'Dien thoai', '1234-12-13', 2);
+INSERT INTO `KHACH_HANG`(`id`, `ten`, `gioi_tinh`, `dia_chi`, `dien_thoai`, `ngay_sinh`, `id_loai_khach_hang`) VALUES
+  (1, 'Tran Quoc Tuan', 'Nam', 'Dia chi', 'Dien thoai', '1234-12-24', 2);
+INSERT INTO `KHACH_HANG`(`id`, `ten`, `gioi_tinh`, `dia_chi`, `dien_thoai`, `ngay_sinh`, `id_loai_khach_hang`) VALUES
+  (2, 'Phan Van Khai', 'Nam', 'Dia chi', 'Dien thoai', '1960-01-01', 2);
+INSERT INTO `KHACH_HANG`(`id`, `ten`, `gioi_tinh`, `dia_chi`, `dien_thoai`, `ngay_sinh`, `id_loai_khach_hang`) VALUES
+  (3, 'Ho Chi Minh', 'Nam', 'Dia chi', 'Dien thoai', '1969-05-19', 2);
+INSERT INTO `KHACH_HANG`(`id`, `ten`, `gioi_tinh`, `dia_chi`, `dien_thoai`, `ngay_sinh`, `id_loai_khach_hang`) VALUES
+  (4, 'Athur', 'Nam', 'Dia chi', 'Dien thoai', '0123-02-14', 1);
+INSERT INTO `KHACH_HANG`(`id`, `ten`, `gioi_tinh`, `dia_chi`, `dien_thoai`, `ngay_sinh`, `id_loai_khach_hang`) VALUES
+  (5, 'Obama', 'Nam', 'Dia chi', 'Dien thoai', '1958-06-01', 1);
+INSERT INTO `KHACH_HANG`(`id`, `ten`, `gioi_tinh`, `dia_chi`, `dien_thoai`, `ngay_sinh`, `id_loai_khach_hang`) VALUES
+  (6, 'Binladen', 'Nam', 'Dia chi', 'Dien thoai', '1957-03-08', 1);
+INSERT INTO `KHACH_HANG`(`id`, `ten`, `gioi_tinh`, `dia_chi`, `dien_thoai`, `ngay_sinh`, `id_loai_khach_hang`) VALUES
+  (7, 'Nguyen Hoang Vu', 'Nam', 'Dia chi', 'Dien thoai', '1988-10-28', 2);
+INSERT INTO `KHACH_HANG`(`id`, `ten`, `gioi_tinh`, `dia_chi`, `dien_thoai`, `ngay_sinh`, `id_loai_khach_hang`) VALUES
+  (8, 'Lam Chi Dinh', 'Nam', 'Dia chi', 'Dien thoai', '1970-01-09', 1);
+INSERT INTO `KHACH_HANG`(`id`, `ten`, `gioi_tinh`, `dia_chi`, `dien_thoai`, `ngay_sinh`, `id_loai_khach_hang`) VALUES
+  (9, 'Mizore-chan', 'Nu', 'Dia chi', 'Dien thoai', '2007-01-01', 1);
+
+
+-- LOAI_THUE
+INSERT INTO `LOAI_THUE`(`id`, `loai`) VALUES
+  (1, 'Dat phong');
+INSERT INTO `LOAI_THUE`(`id`, `loai`) VALUES
+  (2, 'Thue phong');
+
+
+-- TINH_TRANG_PHONG
+INSERT INTO `TINH_TRANG_PHONG`(`id`, `ten`) VALUES
+  (1, 'Con trong');
+INSERT INTO `TINH_TRANG_PHONG`(`id`, `ten`) VALUES
+  (2, 'Da thue');
+
+
+-- LOAI_PHONG
+INSERT INTO `LOAI_PHONG`(`id`, `ten`, `gia`) VALUES
+  (1, 'VIP', 500000);
+INSERT INTO `LOAI_PHONG`(`id`, `ten`, `gia`) VALUES
+  (2, 'Hang trung', 400000);
+INSERT INTO `LOAI_PHONG`(`id`, `ten`, `gia`) VALUES
+  (3, 'Hang thuong', 350000);
+
+
+-- PHONG
+INSERT INTO `PHONG`(`id`, `gia`, `id_tinh_trang`, `id_loai_phong`, `hinh_anh`, `mo_ta`) VALUES
+  (1, null, 1, 1, '', '');
+INSERT INTO `PHONG`(`id`, `gia`, `id_tinh_trang`, `id_loai_phong`, `hinh_anh`, `mo_ta`) VALUES
+  (2, null, 1, 1, '', '');
+INSERT INTO `PHONG`(`id`, `gia`, `id_tinh_trang`, `id_loai_phong`, `hinh_anh`, `mo_ta`) VALUES
+  (3, null, 1, 1, '', '');
+INSERT INTO `PHONG`(`id`, `gia`, `id_tinh_trang`, `id_loai_phong`, `hinh_anh`, `mo_ta`) VALUES
+  (4, null, 2, 1, '', '');
+INSERT INTO `PHONG`(`id`, `gia`, `id_tinh_trang`, `id_loai_phong`, `hinh_anh`, `mo_ta`) VALUES
+  (5, null, 2, 1, '', '');
+INSERT INTO `PHONG`(`id`, `gia`, `id_tinh_trang`, `id_loai_phong`, `hinh_anh`, `mo_ta`) VALUES
+  (6, null, 1, 2, '', '');
+INSERT INTO `PHONG`(`id`, `gia`, `id_tinh_trang`, `id_loai_phong`, `hinh_anh`, `mo_ta`) VALUES
+  (7, null, 1, 2, '', '');
+INSERT INTO `PHONG`(`id`, `gia`, `id_tinh_trang`, `id_loai_phong`, `hinh_anh`, `mo_ta`) VALUES
+  (8, null, 2, 2, '', '');
+INSERT INTO `PHONG`(`id`, `gia`, `id_tinh_trang`, `id_loai_phong`, `hinh_anh`, `mo_ta`) VALUES
+  (9, null, 1, 2, '', '');
+INSERT INTO `PHONG`(`id`, `gia`, `id_tinh_trang`, `id_loai_phong`, `hinh_anh`, `mo_ta`) VALUES
+  (10, null, 1, 2, '', '');
+INSERT INTO `PHONG`(`id`, `gia`, `id_tinh_trang`, `id_loai_phong`, `hinh_anh`, `mo_ta`) VALUES
+  (11, null, 1, 2, '', '');
+INSERT INTO `PHONG`(`id`, `gia`, `id_tinh_trang`, `id_loai_phong`, `hinh_anh`, `mo_ta`) VALUES
+  (12, null, 1, 2, '', '');
+INSERT INTO `PHONG`(`id`, `gia`, `id_tinh_trang`, `id_loai_phong`, `hinh_anh`, `mo_ta`) VALUES
+  (13, null, 1, 2, '', '');
+INSERT INTO `PHONG`(`id`, `gia`, `id_tinh_trang`, `id_loai_phong`, `hinh_anh`, `mo_ta`) VALUES
+  (14, null, 1, 2, '', '');
+INSERT INTO `PHONG`(`id`, `gia`, `id_tinh_trang`, `id_loai_phong`, `hinh_anh`, `mo_ta`) VALUES
+  (15, null, 2, 2, '', '');
+INSERT INTO `PHONG`(`id`, `gia`, `id_tinh_trang`, `id_loai_phong`, `hinh_anh`, `mo_ta`) VALUES
+  (16, null, 2, 3, '', '');
+INSERT INTO `PHONG`(`id`, `gia`, `id_tinh_trang`, `id_loai_phong`, `hinh_anh`, `mo_ta`) VALUES
+  (17, null, 2, 3, '', '');
+INSERT INTO `PHONG`(`id`, `gia`, `id_tinh_trang`, `id_loai_phong`, `hinh_anh`, `mo_ta`) VALUES
+  (18, null, 2, 3, '', '');
+INSERT INTO `PHONG`(`id`, `gia`, `id_tinh_trang`, `id_loai_phong`, `hinh_anh`, `mo_ta`) VALUES
+  (19, null, 2, 3, '', '');
+INSERT INTO `PHONG`(`id`, `gia`, `id_tinh_trang`, `id_loai_phong`, `hinh_anh`, `mo_ta`) VALUES
+  (20, null, 1, 3, '', '');
+INSERT INTO `PHONG`(`id`, `gia`, `id_tinh_trang`, `id_loai_phong`, `hinh_anh`, `mo_ta`) VALUES
+  (21, null, 1, 3, '', '');
+
+
+-- THUE_PHONG
+INSERT INTO `THUE_PHONG`(`id_phong`, `id_khach_hang`, `ngay_thue`, `ngay_tra`, `tong_gia`, `id_loai_thue`) VALUES
+  (1, 2, '2009-12-24', null, null, 1);
+INSERT INTO `THUE_PHONG`(`id_phong`, `id_khach_hang`, `ngay_thue`, `ngay_tra`, `tong_gia`, `id_loai_thue`) VALUES
+  (2, 3, '2009-12-24', null, null, 1);
+INSERT INTO `THUE_PHONG`(`id_phong`, `id_khach_hang`, `ngay_thue`, `ngay_tra`, `tong_gia`, `id_loai_thue`) VALUES
+  (3, 4, '2009-12-24', null, null, 1);
+INSERT INTO `THUE_PHONG`(`id_phong`, `id_khach_hang`, `ngay_thue`, `ngay_tra`, `tong_gia`, `id_loai_thue`) VALUES
+  (3, 5, '2009-12-24', null, null, 1);
+INSERT INTO `THUE_PHONG`(`id_phong`, `id_khach_hang`, `ngay_thue`, `ngay_tra`, `tong_gia`, `id_loai_thue`) VALUES
+  (3, 6, '2009-12-24', null, null, 1);
+INSERT INTO `THUE_PHONG`(`id_phong`, `id_khach_hang`, `ngay_thue`, `ngay_tra`, `tong_gia`, `id_loai_thue`) VALUES
+  (4, 7, '2009-12-24', null, null, 1);
+INSERT INTO `THUE_PHONG`(`id_phong`, `id_khach_hang`, `ngay_thue`, `ngay_tra`, `tong_gia`, `id_loai_thue`) VALUES
+  (5, 8, '2009-12-24', '2009-12-25', null, 1);
+INSERT INTO `THUE_PHONG`(`id_phong`, `id_khach_hang`, `ngay_thue`, `ngay_tra`, `tong_gia`, `id_loai_thue`) VALUES
+  (6, 9, '2009-12-24', null, null, 1);
