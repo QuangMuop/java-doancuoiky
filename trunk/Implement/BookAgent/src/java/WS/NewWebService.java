bd6dbda75847ec019c8ac9b825d41007d08da370
hvu.nguyen2007 <hvu.nguyen2007@ae9a0e02-e63f-11de-b362-3513cf04e977>
2010-01-06 10:07:24 +0000

    them web service vao BookAgent, nhung chua test :D

--- trunk/Implement/BookAgent/src/java/WS/NewWebService.java
+++ trunk/Implement/BookAgent/src/java/WS/NewWebService.java
@@ -5,7 +5,9 @@
 
 package WS;
 
+import Hotel.HotelInfo;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.logging.Level;
@@ -13,6 +15,18 @@ import java.util.logging.Logger;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.xml.sax.SAXException;
 
 /**
  *
@@ -24,29 +38,33 @@ public class NewWebService {
     /**
      * Web service operation
      */
-    @WebMethod(operationName = "HotelReceive")
-    public Boolean HotelReceive(@WebParam(name = "hotelXMLNode")
-    final String hotelXMLNode, @WebParam(name = "hotelDetailXMLFile")
-    final String hotelDetailXMLFile) {
-        
-        BufferedWriter writer = null;
+    @WebMethod(operationName = "operation")
+    public Boolean operation(@WebParam(name = "hotel")
+    final HotelInfo hotel) {
         try {
-            // Save XML content into XML file
-            writer = new BufferedWriter(new FileWriter("newhotel.xml"));
-            writer.write(hotelDetailXMLFile);
-            writer.flush();
-            writer.close();
-            // Save XML document
+            //TODO write your implementation code here:
+            // read from file
+            Document dom = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse("");
+            dom.normalize();
+            dom.getDocumentElement().appendChild(hotel.toXMLNode());
 
+            Transformer transformer = TransformerFactory.newInstance().newTransformer();
+            DOMSource src = new DOMSource(dom.getDocumentElement());
+            StreamResult res = new StreamResult(new File(""));
+
+            transformer.transform(src, res);
+        } catch (TransformerException ex) {
+            Logger.getLogger(NewWebService.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SAXException ex) {
+            Logger.getLogger(NewWebService.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IOException ex) {
             Logger.getLogger(NewWebService.class.getName()).log(Level.SEVERE, null, ex);
-        } finally {
-            try {
-                writer.close();
-            } catch (IOException ex) {
-                Logger.getLogger(NewWebService.class.getName()).log(Level.SEVERE, null, ex);
-            }
+        }
+        catch (ParserConfigurationException ex) {
+            Logger.getLogger(NewWebService.class.getName()).log(Level.SEVERE, null, ex);
         }
         return true;
     }
+
+    
 }
